2a5b1ac31e2f7d844e1e7cb61736f3ef103c4233
ercanemirleroglu <39986316+ercanemirleroglu@users.noreply.github.com>
2023-07-21 10:45:49 +0300

    security cors

--- src/main/java/ist/pinsoft/todo/config/SecurityConfig.java
+++ src/main/java/ist/pinsoft/todo/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/api/**").permitAll()
+                        .requestMatchers("/api/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(AbstractHttpConfigurer::disable)
